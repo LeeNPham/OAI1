c8ea9201512c75a894c755500f8de7600145f171
Lee Pham <leenpham@gmail.com>
2023-07-18 21:29:34 -0700

    moved click indicator

--- Frontend/src/lib/components/ChatMessage.svelte
+++ Frontend/src/lib/components/ChatMessage.svelte
@@ -60,12 +60,7 @@
 
 <!-- svelte-ignore a11y-interactive-supports-focus -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
-<div
-	class="flex {type === 'user' ? 'flex-row-reverse' : 'flex-row'} w-full px-10"
-	role="button"
-	aria-label="Toggle speech"
-	on:click={() => speak()}
->
+<div class="flex {type === 'user' ? 'flex-row-reverse' : 'flex-row'} w-full px-10">
 	<img
 		class="h-14 min-w-14 rounded-full object-cover aspect-square mix-blend-exclusion"
 		src={type === 'user' ? 'https://ui-avatars.com/api/?name=Me' : jarvisDefault}
@@ -77,6 +72,9 @@
 			{type === 'user' ? 'Me' : 'Jarvis'}
 		</div>
 		<div
+			role="button"
+			aria-label="Toggle speech"
+			on:click={() => speak()}
 			class="px-5 py-2 indent-4 {type === 'user'
 				? 'rounded-l-2xl rounded-b-2xl bg-gray-800/50 text-pink-400'
 				: 'rounded-r-2xl rounded-b-2xl bg-gray-800/50 text-cyan-300'}"
