74c04b29f7d662ce49372a07805651f1b5d5adc6
Lee Pham <leenpham@gmail.com>
2023-10-07 13:09:56 -0700

    added a button to make sure that the user interacts with the dom to bypass the autoplay issue for speech synthesis

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,11 +1,41 @@
 <script>
+	import { fly, fade } from 'svelte/transition';
+	import JarvisTriangle from '$lib/components/JarvisTriangle.svelte';
 	import '../app.postcss';
 	import './styles.css';
+	let showJarvis = false;
+	let showOpenButton = false;
 </script>
 
 <div class="app bg-black">
 	<main>
-		<slot />
+		{#if showJarvis == true}
+			<slot />
+		{:else}
+			<div class="relative flex items-center justify-center flex-1">
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+				<div
+					transition:fade
+					on:mouseover={() => {
+						showOpenButton = true;
+					}}
+					class="border-2 border-red-700 rounded-full hover:shadow-2xl hover:shadow-red-700 relative"
+				>
+					<JarvisTriangle />
+					{#if showOpenButton}
+						<button
+							transition:fly={{ x: 200, duration: 1000 }}
+							class="absolute top-0 right-0 mt-16 -mr-40 text-cyan-300 hover:text-white/90 font-bold shadow-md border border-cyan-300/50 hover:bg-cyan-300 shadow-cyan-300 px-4 py-1 rounded-xl"
+							type="button"
+							on:click={() => {
+								showJarvis = true;
+							}}>Open Jarvis</button
+						>
+					{/if}
+				</div>
+			</div>
+		{/if}
 	</main>
 </div>
 
