422566db32977f82b136e2f92810af7b8bc92906
Lee Pham <leenpham@gmail.com>
2023-10-07 10:58:20 -0700

    best so far

--- Frontend/src/lib/components/VoiceRecognition.svelte
+++ Frontend/src/lib/components/VoiceRecognition.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
+	//@ts-nocheck
+	import { onMount, onDestroy } from 'svelte';
 	import { browser } from '$app/environment';
+	import { writable } from 'svelte/store';
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
 
 	let isRecording = false;
 	export let transcript = '';
-	// @ts-ignore
+
 	let recognition: SpeechRecognition | null = null;
+	let wakeWord = 'jarvis'; // Change this to your desired wake word
+	let silenceTimeout: NodeJS.Timeout | null = null;
 
-	//start looking into wake word options for fun
-	$: {
-		if (transcript) {
-			console.log(transcript);
-		}
-	}
+	// Create a writable store for transcript updates
+	let transcriptStore = writable('');
 
+	// Function to start recording
 	const startRecording = () => {
 		if (browser && !isRecording) {
 			isRecording = true;
 			transcript = '';
-			// @ts-ignore
 			recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 			recognition.lang = 'en-US';
 			recognition.continuous = true;
@@ -25,44 +28,37 @@
 
 			recognition.onresult = (event: any) => {
 				const result = event.results[event.results.length - 1];
-				transcript = result[0].transcript;
+				const spokenText = result[0].transcript.toLowerCase();
+
+				if (spokenText.includes(wakeWord)) {
+					clearTimeout(silenceTimeout);
+					transcript = spokenText.substring(spokenText.indexOf(wakeWord));
+					transcriptStore.set(transcript); // Pass the transcript to the store
+				}
+
+				// Reset the silence timeout for each recognized speech
+				clearTimeout(silenceTimeout);
+				silenceTimeout = setTimeout(() => {
+					// If there's silence for 1.5 seconds, stop recognition
+					recognition?.stop();
+					isRecording = false;
+					startRecording(); // Restart recognition
+				}, 1500); // Adjust the duration for the pause before restarting
+			};
+
+			recognition.onend = () => {
+				// Restart recognition when it ends
+				if (isRecording) {
+					startRecording();
+				}
 			};
 
 			recognition.start();
 		}
 	};
 
-	const stopRecording = () => {
-		if (browser && recognition && isRecording) {
-			isRecording = false;
-			recognition.stop();
-		}
-	};
-
-	const clearTranscript = () => {
-		transcript = '';
-	};
+	onMount(() => {
+		// Start recording when the component is mounted
+		startRecording();
+	});
 </script>
-
-<div class="flex flex-col items-end justify-end w-full">
-	<!-- <div class="text-white">Voice Recognition only works with Google Chrome</div> -->
-	<!-- Consider giving this value a wake word so that recording is only occuring for transcribed messages when event is created -->
-	<div>
-		<button
-			class="text-cyan-300 border border-cyan-300 px-2 py-1 w-[140px] rounded-md hover:bg-cyan-300/20 font-bold shadow-md shadow-cyan-300"
-			on:touchstart={startRecording}
-			on:touchend={stopRecording}
-			on:mousedown={startRecording}
-			on:mouseup={stopRecording}
-		>
-			{isRecording ? 'Recording...' : 'Hold to Record'}
-		</button>
-		<button
-			class="text-red-300 border border-cyan-300 px-2 py-1 w-[140px] rounded-md hover:bg-cyan-300/20 font-bold shadow-md shadow-cyan-300"
-			on:click={clearTranscript}
-		>
-			Clear Transcript
-		</button>
-		<!-- This is used to verify the data as being entered <textarea class="text-black" bind:value={transcript} disabled /> -->
-	</div>
-</div>
